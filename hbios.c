7e86e48c0557f22c893dbe5694e44cdc6025c5a1
Dean Netherton <dean.netherton@gmail.com>
2020-03-22 18:10:25 +1100

    HBIOS: Added mocking for BF_SNDVOL and BF_SNDPIT commands

--- hbios.c
+++ hbios.c
@@ -5,6 +5,8 @@
 
 #define BF_SND 0x50
 #define BF_SNDINI (BF_SND + 0)
+#define BF_SNDVOL (BF_SND + 1)
+#define BF_SNDPIT (BF_SND + 2)
 
 void hbiosCall(z80info *z80) {
 
@@ -19,5 +21,16 @@ void hbiosCall(z80info *z80) {
   case BF_SNDINI:
     printf("\r\nHBIOS: BF_SNDINI\r\n");
     break;
+
+  case BF_SNDVOL:
+    printf("\r\nHBIOS: BF_SNDVOL, Driver in C %02X, Channel in D: %02X, Volume in E: %02X\r\n", C, D, E);
+    break;
+
+  case BF_SNDPIT:
+    printf("\r\nHBIOS: BF_SNDPIT, Driver in C %02X, Channel in D: %02X, Pitch in HL: %02X%02X \r\n", C, D, H, L);
+    break;
+
+  default:
+    printf("\r\nHBIOS: %0x", B);
   }
 }
